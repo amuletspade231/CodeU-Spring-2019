1d66428c68fa14cde6d1feffdd43f1b25a14d5e7
Gloria Nduka <glorianduka@Glorias-MacBook-Pro-2.local>
2019-03-03 13:50:42 -0800

    added code

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.codeu.data.Datastore;
+import com.google.codeu.data.User;
 
 /**
  * Handles fetching and saving user data.
@@ -38,11 +39,13 @@ public class AboutMeServlet extends HttpServlet{
    // Request is invalid, return empty response
    return;
   }
-  
-  String aboutMe = "This is " + user + "'s about me.";
-  
-  response.getOutputStream().println(aboutMe);
- }
+    User userData = datastore.getUser(user);
+    if(userData == null || userData.getAboutMe() == null) {
+      return;
+    }
+
+    response.getOutputStream().println(userData.getAboutMe());
+  }
  
  @Override
  public void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -56,11 +59,13 @@ public class AboutMeServlet extends HttpServlet{
   
   String userEmail = userService.getCurrentUser().getEmail();
 
-  System.out.println("Saving about me for " + userEmail);
-  // TODO: save the data
-  
-  response.sendRedirect("/user-page.html?user=" + userEmail);
- }
+  String aboutMe = request.getParameter("about-me");
+
+    User user = new User(userEmail, aboutMe);
+    datastore.storeUser(user);
+
+    response.sendRedirect("/user-page.html?user=" + userEmail);
+  }
 }
   
  
