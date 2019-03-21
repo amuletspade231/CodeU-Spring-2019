0d9b898c8786ed633600d3a6894001494ae777af
Gloria Nduka <glorianduka@Glorias-MacBook-Pro-2.local>
2019-03-21 15:00:06 -0700

    regex

--- src/main/java/com/google/codeu/data/RegexExample.java
+++ src/main/java/com/google/codeu/data/RegexExample.java
@@ -6,9 +6,15 @@ public class RegexExample {
     String regex = "(https?://\\S+\\.(png|jpg|gif))";
     String replacement = "<img src=\"$1\" />";
     
-    String text = "Here is an image: https://example.com/images/cat.jpg";
-    
-    String result = text.replaceAll(regex, replacement);
+    String text = "Here is an image: https://www.youtube.com/watch?v=ETGDCEXnkDM";
+
+   String youtube_regex = "(https://www.youtube.com/watch\\?v=(\\S*))";
+   String youtube_replacement = "<iframe width=\"560\" height=\"315\" "+
+   "src=\"https://www.youtube.com/embed/$2\" frameborder=\"0\" "+
+   "allow=\"accelerometer; autoplay; encrypted-media; gyroscope; "+
+   "picture-in-picture\" allowfullscreen></iframe>"
+
+    String result = text.replaceAll(youtube_regex, youtube_replacement);
     System.out.println(result);
   }
 }
